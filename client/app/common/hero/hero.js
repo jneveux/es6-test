9ff42fcdbd5803360a8a345e44942be32d684107
Jean Neveux <jean.neveux@gmail.com>
2016-03-13 12:36:34 +0100

    Removed Angular Material Changed babel to the latest version Added bootstrap 4

--- client/app/common/hero/hero.js
+++ client/app/common/hero/hero.js
@@ -1,6 +1,5 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
-import material from 'angular-material';
 import heroComponent from './hero.component';
 
 let heroModule = angular.module('hero', [
